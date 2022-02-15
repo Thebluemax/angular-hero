404a9eb3424bf95d4c1176c471fc7ba8928794d2
Maximiliano Fernández <thebluemax13@gmail.com>
2022-02-16 00:22:16 +0100

    test name change

---
+++ src/app/modules/superhero/components/search-form/search-form.component.spec.ts
@@ -0,0 +1,42 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { SearchFormComponent } from './search-form.component';
+
+describe('SearchFormComponent', () => {
+  let component: SearchFormComponent;
+  let fixture: ComponentFixture<SearchFormComponent>;
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ SearchFormComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(SearchFormComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it(`should have a material input`, () => {
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('input[matInput]')).toBeTruthy();
+
+  });
+
+  it('input trigger search emmiter', () => {
+    const term = 'Test Word';
+    component.searchTerm.subscribe( word => {
+      console.log(word)
+      expect(word).toBe(term)
+    } )
+    component.search(term);
+   // fixture.detectChanges();
+   // const compiled = fixture.nativeElement as HTMLElement;
+   // expect(compiled.querySelector('span#main-title')?.textContent).toBe(title);
+  });
+});
