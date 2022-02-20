fa7c67e11e40930c42583c273b76c0ac448319e1
Maximiliano Fernández <thebluemax13@gmail.com>
2022-02-20 01:04:06 +0100

    add delay to server

--- start.sh
+++ start.sh
@@ -4,7 +4,7 @@
 nginx -g 'daemon off;' &
 
 # Start the second process
-echo "\n" | nohup json-server   /var/www/html/assets/backend/db.json --routes /var/www/html/assets/backend/routes.json --no-cors --host 0.0.0.0 --port 3000 > logs.log &
+echo "\n" | nohup json-server   /var/www/html/assets/backend/db.json --routes /var/www/html/assets/backend/routes.json --no-cors --host 0.0.0.0 --port 3000 -d 1000 > logs.log &
 
 # Wait for any process to exit
 wait -n
